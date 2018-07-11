d011ca603da498d5105f9c01f57f4616750d8611
Dirk Rejahl <dirk@digiglu.co.uk>
2018-07-11 17:35:55 +0100

    api wip

--- api/controllers/collection.js
+++ api/controllers/collection.js
@@ -84,7 +84,7 @@ function collectionFind(req, res) {
 
   MongoClient.connect(mongourl, function(err, client) {
     if (err!=null) {
-      logger.warn("collectionFind: DB connection failed", {mongoString: process.env.MONGO_STRING});
+      logger.warn("collectionFind: DB connection failed", {mongoString: process.env.MONGO_STRING, dbname: dbname, error: err});
       res.status(500).send({ error: err });
       return;
     }
